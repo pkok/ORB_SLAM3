c9273ce31836466f1d859c935c0d0cc2e0c6b006
Patrick de Kok <pkok@pm.me>
2021-05-06 12:13:18 +0200

    No visualization for speedup of multiple runs

---
+++ src/HiddenViewer.cc
@@ -0,0 +1,98 @@
+#include "Viewer.h"
+
+#include <mutex>
+
+namespace ORB_SLAM3
+{
+
+Viewer::Viewer(System* pSystem, FrameDrawer *pFrameDrawer, MapDrawer *pMapDrawer, Tracking *pTracking, const string &strSettingPath):
+    both(false), mpSystem(pSystem), mpFrameDrawer(pFrameDrawer),mpMapDrawer(pMapDrawer), mpTracker(pTracking),
+    mbFinishRequested(false), mbFinished(true), mbStopped(true), mbStopRequested(false),
+    mbStopTrack(false)
+{
+}
+
+bool Viewer::ParseViewerParamFile(cv::FileStorage &fSettings)
+{
+    return true;
+}
+
+void Viewer::Run()
+{
+    mbFinished = false;
+    mbStopped = false;
+    while(!CheckFinish()) {
+        mpSystem->ActivateLocalizationMode();
+    }
+    mpSystem->DeactivateLocalizationMode();
+    SetFinish();
+}
+
+void Viewer::RequestFinish()
+{
+    unique_lock<mutex> lock(mMutexFinish);
+    mbFinishRequested = true;
+}
+
+bool Viewer::CheckFinish()
+{
+    unique_lock<mutex> lock(mMutexFinish);
+    return mbFinishRequested;
+}
+
+void Viewer::SetFinish()
+{
+    unique_lock<mutex> lock(mMutexFinish);
+    mbFinished = true;
+}
+
+bool Viewer::isFinished()
+{
+    unique_lock<mutex> lock(mMutexFinish);
+    return mbFinished;
+}
+
+void Viewer::RequestStop()
+{
+    unique_lock<mutex> lock(mMutexStop);
+    if(!mbStopped)
+        mbStopRequested = true;
+}
+
+bool Viewer::isStopped()
+{
+    unique_lock<mutex> lock(mMutexStop);
+    return mbStopped;
+}
+
+bool Viewer::Stop()
+{
+    unique_lock<mutex> lock(mMutexStop);
+    unique_lock<mutex> lock2(mMutexFinish);
+
+    if(mbFinishRequested)
+        return false;
+    else if(mbStopRequested)
+    {
+        mbStopped = true;
+        mbStopRequested = false;
+        return true;
+    }
+
+    return false;
+
+}
+
+void Viewer::Release()
+{
+    unique_lock<mutex> lock(mMutexStop);
+    mbStopped = false;
+}
+
+void Viewer::SetTrackingPause()
+{
+    mbStopTrack = true;
+}
+
+}
+
